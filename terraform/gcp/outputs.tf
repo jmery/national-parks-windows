a751a7f482b31f0dcabf5de892f671316c812011
Jeff Mery <jmery@users.noreply.github.com>
2019-06-12 09:26:12 +1000

    App URL in outputs

--- terraform/gcp/outputs.tf
+++ terraform/gcp/outputs.tf
@@ -1,3 +1,3 @@
-output "national-parks-ip" {
-  value = "${google_compute_instance.national_parks.*.network_interface.0.access_config.0.nat_ip}"
+output "national-parks-url" {
+  value = "http://${google_compute_instance.national_parks.network_interface.0.access_config.0.nat_ip}:8080/national-parks"
 }
